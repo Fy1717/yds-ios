847939226caa81cfc303ee41789ca1ab91eeea40
Furkan Yıldız <frknyldz8489@gmail.com>
2026-02-18 00:19:15 +0300

    Initial Commit

---
+++ yds/ydsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ydsApp.swift
+//  yds
+//
+//  Created by Furkan Yıldız on 18.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ydsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
